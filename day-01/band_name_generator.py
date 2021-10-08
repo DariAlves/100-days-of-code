fcc8032409002acf7aa2dca84a06a1ce32ce70f0
Dari Alves <darieltondf@gmail.com>
2021-10-08 13:21:47 -0300

    day 01 projeto

---
+++ day-01/band_name_generator.py
@@ -0,0 +1,6 @@
+# Project 01
+
+print("Hi There! Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of a pet? \n")
+print(f"Your Band's name is {city.capitalize()} {pet.capitalize()}!")
