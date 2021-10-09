23f586d53fac78b34bd4f7e4147ad9e34e79750a
Dari Alves <darieltondf@gmail.com>
2021-10-09 09:32:37 -0300

    math operations

---
+++ day-02/math_operations.py
@@ -0,0 +1,10 @@
+# PEMDAS (Left to Right)
+# Parentheses  ()
+# Exponents **
+# Multiplication * 
+# Division /
+# Addition +
+# Subtraction -
+
+print(3 * 3 + 3 / 3 - 3) #output: 7.0
+print(3 * (3 + 3) / 3 - 3) #output: 3.0
