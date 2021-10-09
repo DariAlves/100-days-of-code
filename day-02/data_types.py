564771158f563a6cdd87a3306d85af7ce5a58c5d
Dari Alves <darieltondf@gmail.com>
2021-10-09 09:04:11 -0300

    data types

---
+++ day-02/data_types.py
@@ -0,0 +1,16 @@
+# Data Types
+
+#String
+print("python"[-1])
+
+#Integer
+print(5 + 10)
+
+print(123_456) # Python ignores this _ (underscore). To make the large number more readable.
+
+#Float
+print(3.14)
+
+#Boolean
+print(True)
+print(False)
