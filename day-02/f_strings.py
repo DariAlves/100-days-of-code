888343f6f9184b0d69d4d438a7d01e670e408466
Dari Alves <darieltondf@gmail.com>
2021-10-09 10:27:56 -0300

    f-strings and numbers manipulation

---
+++ day-02/f_strings.py
@@ -0,0 +1,28 @@
+# Number manipulation 
+
+n = 8 / 3
+print(n) #2.666...
+
+round_n = round(n)
+print(round_n) #3
+
+round_n2 = round(n,2)
+print(round_n2) # 2.67
+
+n2 = 8 / 3
+print(n2) #2.666... float
+
+n3 = 8 // 3
+print(n3) #2 integer
+
+score = 0
+
+# User score a point
+score += 1 # score = score + 1 (short)
+
+print(score) 
+
+#f-String
+height = 1.80
+weight = 75
+print(f"your height is {height} and your weight is {weight}")
