ca6adba1526b9e175674e6342528d1e264f387bb
Dari Alves <darieltondf@gmail.com>
2021-10-10 17:00:56 -0300

    if else

---
+++ day-03/if_else.py
@@ -0,0 +1,14 @@
+# if /else
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride!")
+
+# Shorthand - all in the same line
+# print("You can ride the rollercoaster!") if height >= 120 else print("Sorry, you have to grow taller before you can ride!")
+
+
