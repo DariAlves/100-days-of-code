a7009c24a6f4a14f8556e0c89344a2cbaf0d6248
Dari Alves <darieltondf@gmail.com>
2021-10-12 18:42:23 -0300

    banker roulette

---
+++ day-04/banker_roulette.py
@@ -0,0 +1,12 @@
+import random
+
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+
+random_index = random.randint(0, (len(names) - 1))
+random_name = names[random_index]
+
+print(f"{random_name} is going to buy the meal today!")
+
+# solution 2
+# print(random.choice(names))
