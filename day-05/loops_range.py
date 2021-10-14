0b5a82cc6994d054ff08ba327b85e36acaf01417
Dari Alves <darieltondf@gmail.com>
2021-10-14 08:55:24 -0300

    for loops with range

---
+++ day-05/loops_range.py
@@ -0,0 +1,13 @@
+# For loop with range
+
+# for number in range(0, 11): # not included the last digit
+#     print(number)
+
+# for number2 in range(0, 11, 2):
+#     print(number2)
+
+total = 0
+for number in range(0, 101):
+    total += number
+
+print(total)
