5233c153a69069fd235403e33308c702590a14c1
Dari Alves <darieltondf@gmail.com>
2021-10-16 21:16:34 -0300

    reeborg's world hurdle 1

---
+++ day-06/Hurdle_1.py
@@ -0,0 +1,29 @@
+def turn_around():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()   
+    turn_around()
+    move()
+    turn_around()
+    move()
+    while not wall_in_front():
+        move()
+    turn_left()
+while not at_goal():
+    jump() if not front_is_clear() else move()
+    
+        
+        
+
+
+
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
