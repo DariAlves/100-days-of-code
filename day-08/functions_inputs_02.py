f040b17c35f429e47c1440e95288b020852e1945
Dari Alves <darieltondf@gmail.com>
2021-10-18 20:06:54 -0300

    positional and keywords arguments

---
+++ day-08/functions_inputs_02.py
@@ -0,0 +1,9 @@
+def greet(name, location):
+    print(f"Hello, {name}")
+    print(f"What is it like in {location}?\n")
+
+# positional arguments
+greet("Gotham", "Barbara Gordon") 
+
+# keyword argument
+greet(location="Gotham", name="Barbara Gordon") 
