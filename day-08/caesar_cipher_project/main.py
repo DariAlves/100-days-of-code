3284b3b956b3cacc2287cf193aec803401f53b86
Dari Alves <darieltondf@gmail.com>
2021-10-20 21:33:07 -0300

    day 08 project

--- day-08/caesar_cipher_project/main.py
+++ day-08/caesar_cipher_project/main.py
@@ -1,27 +1,35 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from art import logo
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+print(logo)
 
 def caesar(text, shift, direction):
     text_list = list(text)
     encoded_text = ''
     for letter in text_list:
-        index = alphabet.index(letter)
-
-        if direction == 'encode':
-            new_index = index + shift
-        elif direction == 'decode':
-            new_index = index - shift
-
-        if new_index < 26:
-            letter = alphabet[new_index]
-            encoded_text += letter
+        if letter in alphabet:
+            index = alphabet.index(letter)
+            if direction == 'encode':
+                new_index = index + shift
+            elif direction == 'decode':
+                new_index = index - shift
+            encoded_text += alphabet[new_index]
         else:
-            letter = alphabet[new_index - len(alphabet)] # returns to the position 0
             encoded_text += letter
 
     print(f"The {direction} text is {encoded_text}")
 
-caesar(text, shift, direction)
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    caesar(text, shift, direction)
+
+    response = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if response == "no":
+        print("Goodbye!")
+        break
+        
+    # bug 'yes' or 'no'
