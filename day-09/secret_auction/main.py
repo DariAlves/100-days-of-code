2ec2ebfe13ba179ccf26c29b6372691e8beaa3da
Dari Alves <darieltondf@gmail.com>
2021-10-22 19:14:56 -0300

    day 09 project

--- day-09/secret_auction/main.py
+++ day-09/secret_auction/main.py
@@ -1,2 +1,46 @@
-# Project 09
-print("Project 09")
+from art import logo
+from os import system
+
+print(logo)
+
+# Clear console 
+clear = lambda: system('clear')
+
+bidders_data = []
+
+def add_bidders_data(name, bid):   
+    bidders_data.append(
+        {
+            "name": name,
+            "bid": bid
+        }
+    )
+
+while True:
+    name = input("What is your name? ")
+    bid = int(input("What is your bid? $"))
+    other_bidders = input("Are there any other bidders? Type 'yes or 'no'. ").lower()
+
+    if other_bidders == "yes":
+        clear()
+        add_bidders_data(name, bid)
+        # continue
+    elif other_bidders == "no":
+        add_bidders_data(name, bid)
+        break
+
+final_bid = 0
+winner = ''
+
+for bidder in bidders_data:
+    bidder_name = bidder["name"]
+    bid_value = bidder["bid"]
+    if bid_value > final_bid:
+        final_bid = bid_value
+        winner = bidder_name
+        
+print(f"The winner is {winner} with a bid of ${final_bid}")
+
+
+
+
